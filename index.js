cb0b45863c8628f074b5341955f18633939b0f77
Leon <hualiang.tian@gmail.com>
2021-09-26 22:06:24 -0400

    commit tree 1d87e35c20c5e16c947364d3ebf91606a96a2baa

---
+++ index.js
@@ -0,0 +1,25 @@
+// let divide = function()
+// {
+//     let a = 2000;
+//     let b = 100; 
+//     return a / b ;
+// }
+ 
+// function divide()
+// {
+//     let a = 2000;
+//     let b = 100; 
+//     return a / b ;
+// }
+
+let divide = div => 2000 / 100 ;
+
+
+// let square = function(a)
+// {
+//     return a *a; 
+// }
+
+let square = sq => sq*sq;
+
+let add = (a,d) => a+d; 
